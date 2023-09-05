cbb0e58b60751e392ef6fd8fb78e826ed497127b
JnerdQ <julyanyto@gmail.com>
2023-09-04 22:50:09 -0500

    Cambio diseño y colores login

--- src/Componentes/Login.tsx
+++ src/Componentes/Login.tsx
@@ -1,42 +1,42 @@
 import React, { useState, FormEvent } from "react";
 
 const Login: React.FC = () => {
-  const [email, setEmail] = useState<string>("");
+  const [documento, setDocumento] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
+    console.log(`documento: ${documento}, Password: ${password}`);
   };
 
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center text-center bg-neutral-50 px-4">
+      <div className="min-h-screen flex flex-col items-center justify-center text-center bg-white px-4">
         <div className="mb-8 text-center px-4">
           <div className="mb-8 text-center px-4">
-            <h1 className="text-blue-500 font-extrabold mb-12  text-3xl sm:text-4xl">
+            <h1 className="text-slate-800 font-extrabold mb-12  text-2xl sm:text-3xl">
               TALLER AUTOMOTRIZ RUNFIX
             </h1>
           </div>
         </div>
-        <div className="w-full max-w-xs sm:max-w-md bg- rounded-3xl shadow-2xl p-8">
-          <h2 className="text-2xl font-extrabold text-blue-400 mb-6">
+        <div className="w-full max-w-xs sm:max-w-md  rounded-3xl shadow-2xl p-8 bg-slate-800">
+          <h2 className="text-2xl font-extrabold text-blue-500 mb-6">
             Iniciar sesión
           </h2>
           <form onSubmit={handleSubmit}>
             <div className="mb-6">
-              <label htmlFor="email" className="sr-only">
-                Correo electrónico
+              <label htmlFor="documento" className="sr-only">
+                Documento de identidad (ID)
               </label>
               <input
-                id="email"
-                name="email"
-                type="email"
+                id="documento"
+                name="documento"
+                type="documento"
                 required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full px-3 py-4 placeholder-gray-400 border rounded-md focus:outline-none focus:border-blue-600 focus:ring focus:ring-blue-200"
-                placeholder="Correo electrónico"
+                value={documento}
+                onChange={(e) => setDocumento(e.target.value)}
+                className="w-full px-3 py-4 placeholder-gray-600 border rounded-xl focus:outline-none focus:ring focus:ring-blue-500"
+                placeholder="Documento"
               />
             </div>
             <div className="mb-6">
@@ -50,16 +50,17 @@ const Login: React.FC = () => {
                 required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-3 py-4 placeholder-gray-400 border rounded-md focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200"
+                className="w-full px-3 py-4 placeholder-gray-600 border rounded-2xl focus:outline-none focus:ring focus:ring-blue-500"
                 placeholder="Contraseña"
               />
             </div>
             <div className="mb-6">
               <button
                 type="submit"
-                className="w-full h-12 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring focus:ring-blue-200"
+                className="w-full h-12 bg-blue-500 text-white p-2 rounded-2xl
+                 hover:bg-blue-700 focus:outline-none focus:border-blue-900 focus:ring focus:ring-blue-200"
               >
-                Iniciar sesión
+                Ingresar
               </button>
             </div>
           </form>
