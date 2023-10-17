41e0907c0cba306a4bbbd89ccad0fdeae227da49
JnerdQ <julyanyto@gmail.com>
2023-10-17 11:51:55 -0500

    Prueba 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 // Autor: RunFixTeam
-import RoutesApp from './routes/Router';
+//import RoutesApp from './routes/Router';
+import Home from './pages/Home';
 
 function App() {
-  return <RoutesApp />;
+  return <Home />;
 }
 
 export default App;
