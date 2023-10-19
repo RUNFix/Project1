333a6ce9c2d0cec56fbc08ec6f8d01ef3f9cd9d2
JnerdQ <julyanyto@gmail.com>
2023-10-19 11:19:16 -0500

    Mis cambios a partir del commit antiguo

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,7 @@
-import { Request,Response,Router } from "express";
-import {registerCtrl, loginCtrl, updatePassCtrl} from "../controllers/auth"
+import { Request, Response, Router } from "express";
+import { registerCtrl, loginCtrl, updatePassCtrl } from "../controllers/auth";
 
-
-const router = Router()
+const router = Router();
 /*http://localhost:4000/auth/login [POST] */
 router.post("/register", registerCtrl);
 router.post("/login", loginCtrl);
